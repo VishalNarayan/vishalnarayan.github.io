911e649a92b10dbefdf57cb72424319cbf656f56
Vishal Narayan <nvnaraya@amazon.com>
2023-10-12 16:10:29 -0700

    add source files.

---
+++ source/build-site.el
@@ -0,0 +1,17 @@
+;; Load the publishing system
+(require 'ox-publish)
+
+;; Define the publishing project
+(setq org-publish-project-alist
+      (list
+       (list "website"
+	     :recursive t
+	     :base-directory "~/Documents/website"
+	     :publishing-directory "~/Documents/vishalnarayan.github.io/docs"
+	     :publishing-function 'org-html-publish-to-html
+	     :exclude "*~")))
+
+;; Generate the site output
+(org-publish-all t)
+
+(message "Build complete!")
